8cef1232c010a8e8480b1e2eb78a45de558c3fde
Tristan Long <longtw@umich.edu>
2022-12-20 22:24:50 -0500

    Created a level class

--- game.js
+++ game.js
@@ -9,3 +9,25 @@ let simpleLevelPlan = `
 ......##############..
 ......................`;
 
+
+/*
+Class to store level objects taking a string as an input to define level strucutre.
+*/
+class Level {
+    constructor(plan) {
+        let rows = plan.trim().split("\n").map(l => [...l]);
+        this.height = rows.length;
+        this.width = rows[0].length;
+        this.startActors = [];
+
+        this.rows = rows.map((row, y) => {
+            return row.map((ch, x) => {
+              let type = levelChars[ch];
+              if (typeof type == "string") return type;
+              this.startActors.push(
+                type.create(new Vec(x, y), ch));
+              return "empty";
+            });
+          });
+        }
+      }
