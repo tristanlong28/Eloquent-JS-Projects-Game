ff64c4ad2a1a28e22d5eff1962bd9a9da6fc93bb
Tristan Long <longtw@umich.edu>
2022-12-20 22:34:32 -0500

    Added the Vec class used for position and size of game actors

--- game.js
+++ game.js
@@ -47,3 +47,15 @@ class State {
         return this.actors.find(a => a.type == "player");
     }
 }
+
+class Vec {
+    constructor(x, y) {
+        this.x = x; this.y = y;
+    }
+    plus(other) {
+        return new Vec(this.x + other.x, this.y + other.y);
+    }
+    times(factor) {
+        return new Vec(this.x * factor, this.y * factor);
+    }
+}
