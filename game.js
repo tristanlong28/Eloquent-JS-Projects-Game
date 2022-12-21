4be603865e2da360987e46d16c8866e6f7775de4
Tristan Long <longtw@umich.edu>
2022-12-20 22:30:14 -0500

    Created the State class to track the state of a running game

--- game.js
+++ game.js
@@ -21,13 +21,29 @@ class Level {
         this.startActors = [];
 
         this.rows = rows.map((row, y) => {
-            return row.map((ch, x) => {
-              let type = levelChars[ch];
-              if (typeof type == "string") return type;
-              this.startActors.push(
-                type.create(new Vec(x, y), ch));
-              return "empty";
+                return row.map((ch, x) => {
+                let type = levelChars[ch];
+                if (typeof type == "string") return type;
+                this.startActors.push(
+                    type.create(new Vec(x, y), ch));
+                return "empty";
             });
-          });
-        }
-      }
+        });
+    }
+}
+
+class State {
+    constructor(level, actors, status) {
+        this.level = level;
+        this.actors = actors;
+        this.status = status;
+    }
+
+    static start(level) {
+        return new State(level, level.startActors, "playing");
+    }
+
+    get player() {
+        return this.actors.find(a => a.type == "player");
+    }
+}
