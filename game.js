9f75d723f72d395c754f8de057b5a365e1a3ec95
Tristan Long <longtw@umich.edu>
2022-12-24 13:08:14 -0500

    Completed collision functionality

--- game.js
+++ game.js
@@ -196,3 +196,58 @@ DomDisplay.prototype.scrollPlayerIntoView = function(state) {
         this.dom.scrollTop = center.y + margin - height;
     }
 };
+
+/*Colision and motion section----------------------------------------------------*/
+Level.prototype.touches = function(pos, size, type) {
+    let xStart = Math.floor(pos.x);
+    let xEnd = Math.ceil(pos.x + size.x);
+    let yStart = Math.floor(pos.y);
+    let yEnd = Math.ceil(pos.y + size.y);
+
+    for (let y = yStart; y < yEnd; y++) {
+        for (let x = xStart; x < xEnd; x++) {
+            let isOutside = x < 0 || x >= this.width ||
+                            y < 0 || y >= this.height;
+            let here = isOutside ? "Wall" : this.rows[y][x];
+            if (here == type) return true;
+        }
+    }
+    return false;
+};
+
+State.prototype.update = function(time, keys) {
+    let actors = this.actors.map(actor => actor.update(time, this, keys));
+    let newState = new State(this.level, actors, this.status);
+
+    if (newState.status != "playing") return newState;
+
+    let player = newState.player;
+    if (this.level.touches(player.pos, player.size, "lava")) {
+        return new State(this.level, actors, "lost");
+    }
+
+    for (let actor of actors) {
+        if (Actor != player && overlap(actor, player)) {
+            newState = actor.collide(newState);
+        }
+    }
+    return newState;
+};
+
+function ovelrap(actor1, actor2) {
+    return  actor.pos.x + actor1.size.x > actor2.pos.x &&
+            actor.pos.x < actor2.pos.x + actor2.size.x &&
+            actor1.pos.y + actor1.size.y > actor2.pos.y &&
+            actor1.pos.y < actor2.pos.y + actor2.size.y;
+}
+
+Lava.prototype.collide = function(state) {
+    return newState(State.level, state.actors, "lost");
+};
+
+Coin.prototype.collide = function(state) {
+    let filtered = state.actors.filter(a => a != this);
+    let status = state.status;
+    if (!filtered.some(a => a.type == "coin")) status = "won";
+    return new State(state.level, filtered, status);
+};
