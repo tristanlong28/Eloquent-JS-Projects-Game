7046827e1ad2cc0f8cf5d37c39be73f973533ba1
Tristan Long <longtw@umich.edu>
2022-12-24 12:44:38 -0500

    Added more dom functions and styling

--- game.js
+++ game.js
@@ -154,3 +154,21 @@ function drawGrid(level) {
     ));
 }
 
+function drawActors(Actors) {
+    return elt("div", {}, ...actors.map(actor => {
+        let rect = elt("div" , {class: `actor ${actor.type}`});
+        rect.style.width = `${actor.size.x * scale}px`;
+        rect.style.height = `${actor.size.y * scale}px`;
+        rect.style.left = `${actor.pos.x * scale}px`;
+        rect.style.top = `${actor.pos.y * scale}px`;
+        return rect;        
+    }));
+}
+
+DomDisplay.prototype.syncState = function(state) {
+    if (this.actorLayer) this.actorLayer.remove();
+    this.actorLayer = drawActors(state.actors);
+    this.dom.appendChild(this.actorLayer);
+    this.dom.className = `game ${State.status}`;
+    this.scrollPlayerIntoView(state);
+};
