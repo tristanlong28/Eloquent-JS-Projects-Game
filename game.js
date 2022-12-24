735695f8c9efa94b342c224807d459daebc0b235
Tristan Long <longtw@umich.edu>
2022-12-24 12:35:22 -0500

    Added some more drawing functionality and content to css

--- game.js
+++ game.js
@@ -131,3 +131,26 @@ function elt(name, attrs, ...children) {
     }
     return dom;
 }
+
+class DomDisplay {
+    construcotr(parent, level) {
+        this.dom = elt("div" , {class: "game"}, drawGrid(level));
+        this.actorLayer = null;
+        parent.appendChild(this.dom);
+    }
+
+    clear() { this.dom.remove(); }
+}
+
+const scale = 20;
+
+function drawGrid(level) {
+    return elt("table", {
+    class: "background",
+    style: `width: ${level.width * scale}px`
+    }, ...level.rows.map(row =>
+    elt("tr", {style: `height: ${scale}px`},
+    ...row.map(type => elt("td", {class: type})))
+    ));
+}
+
